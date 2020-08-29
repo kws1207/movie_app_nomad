9afa0fe2f0f0844d41883503c9f591829208008e
김우성 <kws@gim-useong-ui-MacBookPro.local>
2020-08-29 21:35:27 +0900

    Updates

---
+++ precache-manifest.7b73d3d209e8db4ef635a557b713f2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c245807747514f878e89a186bbdbc7f3",
+    "url": "/movie_app_nomad/index.html"
+  },
+  {
+    "revision": "0fb19fe95846c77d8b77",
+    "url": "/movie_app_nomad/static/css/main.33ae50bc.chunk.css"
+  },
+  {
+    "revision": "e29617e6b39dc4d5a110",
+    "url": "/movie_app_nomad/static/js/2.6203725a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_nomad/static/js/2.6203725a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb19fe95846c77d8b77",
+    "url": "/movie_app_nomad/static/js/main.069a70f4.chunk.js"
+  },
+  {
+    "revision": "589abfb683ef8f0720a3",
+    "url": "/movie_app_nomad/static/js/runtime-main.808ae562.js"
+  }
+]);
